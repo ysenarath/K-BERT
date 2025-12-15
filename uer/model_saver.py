25a00d56497140bfbda0a7ad8814d610ac2d3eb1
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-15 01:54:41 -0500

    Enhance type hinting in model_saver.py for improved clarity and type safety

--- uer/model_saver.py
+++ uer/model_saver.py
@@ -1,11 +1,19 @@
 # -*- encoding:utf-8 -*-
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Protocol, runtime_checkable
+
 import torch
-import collections
 
 
-def save_model(model, model_path):
-    # We dont't need prefix "module".
-    if hasattr(model, "module"):
+@runtime_checkable
+class Model(Protocol):
+    module: torch.nn.Module
+
+
+def save_model(model: Model | torch.nn.Module, model_path: str | Path):
+    if isinstance(model, Model):
         torch.save(model.module.state_dict(), model_path)
     else:
         torch.save(model.state_dict(), model_path)
