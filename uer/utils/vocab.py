3486b87a1044a927d321c4073fb3c269210664b8
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-15 01:54:30 -0500

    Refactor vocab.py for improved type hinting and code readability

--- uer/utils/vocab.py
+++ uer/utils/vocab.py
@@ -1,11 +1,14 @@
 # -*- encoding:utf-8 -*-
+from __future__ import annotations
+
 import os
-import torch
-from uer.utils.constants import *
 from multiprocessing import Pool
+from pathlib import Path
+
+from uer.utils.constants import UNK_ID
 
 
-def count_line(corpus_path):
+def count_line(corpus_path: Path | str) -> int:
     count = 0
     with open(corpus_path, mode="r", encoding="utf-8") as f:
         for line in f:
@@ -14,15 +17,16 @@ def count_line(corpus_path):
 
 
 class Vocab(object):
-    """
-    """
+    """ """
+
     def __init__(self):
-        self.w2i = {} 
-        self.i2w = [] 
-        self.w2c = {} 
-        self.reserved_vocab_path = \
-            os.path.abspath(os.path.join(os.path.dirname(__file__), "../../models/reserved_vocab.txt"))
-        
+        self.w2i = {}
+        self.i2w = []
+        self.w2c = {}
+        self.reserved_vocab_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../../models/reserved_vocab.txt")
+        )
+
     def load(self, vocab_path, is_quiet=False):
         with open(vocab_path, mode="r", encoding="utf-8") as reader:
             for index, line in enumerate(reader):
@@ -30,11 +34,11 @@ class Vocab(object):
                     w = line.strip().split()[0]
                     self.w2i[w] = index
                     self.i2w.append(w)
-                except:
-                    self.w2i["???"+str(index)] = index
-                    self.i2w.append("???"+str(index))
+                except Exception:
+                    self.w2i["???" + str(index)] = index
+                    self.i2w.append("???" + str(index))
                     if not is_quiet:
-                        print("Vocabulary file line " + str(index+1) + " has bad format token")
+                        print(f"Vocabulary file line {index + 1} has bad format token")
             assert len(self.w2i) == len(self.i2w)
         if not is_quiet:
             print("Vocabulary Size: ", len(self))
@@ -48,31 +52,31 @@ class Vocab(object):
 
     def get(self, w):
         return self.w2i.get(w, UNK_ID)
-        
+
     def __len__(self):
         return len(self.i2w)
-        
+
     def worker(self, corpus_path, tokenizer, start, end):
-        """ 
+        """
         Worker that creates vocabulary from corpus[start:end].
         """
         w2i, i2w, w2c = {}, [], {}
         pos = 0
         with open(corpus_path, mode="r", encoding="utf-8") as f:
             while pos < start:
-               try:
-                   f.readline()
-               except:
-                   continue
-               finally:
-                   pos += 1
+                try:
+                    f.readline()
+                except:
+                    continue
+                finally:
+                    pos += 1
             while True:
                 try:
                     line = f.readline()
                 except:
                     continue
                 finally:
-                   pos += 1
+                    pos += 1
 
                 tokens = tokenizer.tokenize(line)
                 for t in tokens:
@@ -83,9 +87,9 @@ class Vocab(object):
                         w2c[t] += 1
                 if pos >= end - 1:
                     return (w2i, i2w, w2c)
-                            
+
     def union(self, vocab_list):
-        """ Union vocab in all workers. """
+        """Union vocab in all workers."""
         w2i, i2w, w2c = {}, [], {}
         index = 0
         for v_p in vocab_list:
@@ -97,25 +101,27 @@ class Vocab(object):
                 else:
                     w2c[w] += w2c_p[w]
         return (w2i, i2w, w2c)
-                    
+
     def build(self, corpus_path, tokenizer, workers_num=1, min_count=1):
-        """ Build vocabulary from the given corpus. """
+        """Build vocabulary from the given corpus."""
         print("Start %d workers for building vocabulary..." % workers_num)
         lines_num = count_line(corpus_path)
         pool = Pool(workers_num)
         vocab_list = []
         for i in range(workers_num):
             start = i * lines_num // workers_num
-            end = (i+1) * lines_num // workers_num
-            vocab_p = pool.apply_async(func=self.worker, args=[corpus_path, tokenizer, start, end])
+            end = (i + 1) * lines_num // workers_num
+            vocab_p = pool.apply_async(
+                func=self.worker, args=[corpus_path, tokenizer, start, end]
+            )
             vocab_list.append(vocab_p.get())
         pool.close()
         pool.join()
-        
+
         # Union vocab in all workers.
         w2i, i2w, w2c = self.union(vocab_list)
         # Sort w2c according to word count.
-        sorted_w2c = sorted(w2c.items(), key=lambda item:item[1], reverse=True)
+        sorted_w2c = sorted(w2c.items(), key=lambda item: item[1], reverse=True)
 
         # Add special symbols and remove low frequency words.
         with open(self.reserved_vocab_path, mode="r", encoding="utf-8") as reader:
@@ -131,4 +137,3 @@ class Vocab(object):
             if w not in self.w2i:
                 self.w2i[w], self.w2c[w] = len(self.i2w), c
                 self.i2w.append(w)
-                
